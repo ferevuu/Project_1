96eca3ddbc1b39115cc5b150996a1c9bb217f58e
ferevuu <frantisekvujtek@seznam.cz>
2023-10-15 18:28:08 +0200

    almost_final

--- Engeto_Text_Analyser.py
+++ Engeto_Text_Analyser.py
@@ -42,17 +42,14 @@ uzivatele = {"bob": "123",
              "liz": "pass123"
             }
 
-
-
-
-
 # Proměné k verifikaci loginu
 username = input("Please, log in with your username: ")
 password = input("Please verify with your password: ")
 print(cara)
 
 # Podmínky k verifikaci
-if username in uzivatele and uzivatele[username] == password: #Pokud jmeno a heslo jsou stejne, může dál
+if username in uzivatele and uzivatele[username] == password: # Pokud jmeno a heslo jsou stejne, může dál
+    print("Welcome to the app,", username)
     print("Thanks for the verification, here is the TEXT offer to analyze:")
     print(cara)      
 
@@ -66,18 +63,67 @@ for index, texty in enumerate(TEXTS, start=1): # Rozdělí texty přehledně pod
     print(f"Text {index}:\n{texty}\n") 
     continue
 
-
+# Výběr z nabídky textů 1 až 3
 while True:
-    a_text = int(input("Enter the number of TEXT you want to Analyze (1 - 3): "))
-    if 1 <= a_text <= 3:
-        vyber = TEXTS[a_text - 1]
-        print(cara)
-        print(f"analyzing\n {vyber}")
-        break
+    a_text = input("Enter the number of TEXT you want to Analyze (1 - 3): ")
+    if a_text.isdigit(): # Ověruje, zda je input číslo
+        a_text = int(a_text)
+        if 1 <= a_text <= 3: 
+            vyber = TEXTS[int(a_text) - 1]
+            print(cara)
+            print(f"Analyzing Text :\n{vyber}")
+            break
+        else:
+            print("Invalid Input, You have to choose number between 1 and 3")
+            quit()
     else:
-        print("Invalid Input, You have to choose number betwwen 1 and 3")
-        break
+        print("Invalid Input, You have to choose number between 1 and 3")
+        quit()   
     
+# Očištění textu o speciální znaky a rozdělit podle mezer
+clean_vyber = "".join(znak for znak in vyber if znak not in (",.?!:;_"))
+slova = clean_vyber.split()
+pocet_slov = len(slova)
+
+
+velka_pismena_zac = [slovo for slovo in slova if slovo.istitle()]
+male_pismena = [slovo for slovo in slova if slovo.islower()]
+velka_pismena = [slovo for slovo in slova if slovo.isupper()]
+pocet_cisel = [slovo for slovo in slova if slovo.isnumeric()]
+suma_cisel = sum(int(suma) for suma in pocet_cisel if suma.isnumeric())
+
+# Vypsání statistik textu:
+print(cara)
+print(f"Počet slov v textu: {pocet_slov}")
+print(f"Počet slov začínajících velkým písmenem: {len(velka_pismena_zac)}")
+print(f"Počet slov psaných velkými písmeny: {len(velka_pismena)}")
+print(f"Počet slov psaných malými písmeny: {len(male_pismena)}")
+print(f"Počet čísel v textu: {len(pocet_cisel)}")
+print(f"Součet všech čísel v textu: {suma_cisel}")
+print(cara)
+
+
+delka_slov = {}
+
+for delka in slova:
+    slovoo = len(delka)
+    if slovoo not in delka_slov:
+        delka_slov[slovoo] = 1
+    else:
+        delka_slov[slovoo] += 1
+
+serazene_slova = sorted(delka_slov.items())
+
+print("Délka | Výskyt | Počet ")
+print("-" * 30)
+
+for delka, pocet in serazene_slova:
+    hvezdy = "*" * pocet
+    print(f" {delka:2}|{hvezdy:20}|{pocet:2}")
+
+
+
+
 
 
 
