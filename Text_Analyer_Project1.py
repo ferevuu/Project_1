0d6fdc6676e66bd1a6e5a5f7502d81eb357cfa39
ferevuu <frantisekvujtek@seznam.cz>
2023-10-21 20:29:53 +0200

    Final

---
+++ Text_Analyer_Project1.py
@@ -0,0 +1,95 @@
+from pprint import pprint 
+'''
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+author: František Vůjtek
+email: frantisekvujtek@seznam.cz
+discord: fere_91948
+'''
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+
+# Základní proměnné
+cara = "=" * 40
+uzivatele = {"bob": "123",
+             "ann": "pass123",
+             "mike": "password123", 
+             "liz": "pass123"
+            }
+
+# Login a Verifikace Usera v loopu dokud nezadá správné heslo
+while True:
+    username = input("Please Login: ")
+    password = input("Pleaste Enter your password: ")
+    if username in uzivatele and uzivatele[username] == password:
+        print("Welcome to the Text Analyzer! ", username)
+        break
+    else: 
+        print("Wrong Username or Password, Try again!".upper())
+
+# Výběr z nabídky textu 1 až 3
+
+print(cara)
+
+for idx, text in enumerate(TEXTS, 1):
+    print(f"Text {idx} to Analyze:\n {text}\n")
+
+
+
+# Vybírání textu k Analýze
+while True:
+
+    vyber = input("Choose the Text you want to Analyze (1 - 3): ")
+    if vyber.isdigit():
+        if int(vyber) >= 1 and int(vyber) < 3:
+            print(cara)
+            textik = TEXTS[int(vyber) - 1]
+            print(f"Analyzing Text: {vyber}\n{textik}\n")
+            break
+        else:
+            print("You have to choose between number 1 - 3")
+            continue
+
+    else:
+        print("You have to choose number between 1 - 3, Don´t enter Text here!")
+        print("Closing Analyzer")
+        quit()
+
+
+# Očištění textu o speciální znaky a rozdělení podle mezer
+
+
+
+cisty_text = "".join(znak for znak in text.strip(,.?:!)).split(",")
+print(cisty_text)
+
+
+# Vypsaní statistik textu a for cykly pro ně
+
+# Sekce pro výpočet grafu, který indikuje četnost a počet písmen v textu
+
