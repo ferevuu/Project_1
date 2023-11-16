9e087dc41cf32df8327f10125a867fdf237fcb06
ferevuu <frantisekvujtek@seznam.cz>
2023-11-16 08:46:52 +0100

    Finalni_bulls

--- BullandCows.py
+++ BullandCows.py
@@ -1,13 +1,15 @@
 """
 projekt_2.py: druhý projekt do Engeto Online Python Akademie
 
-author: Petr Svetr
-email: petr.svetr@gmail.com
-discord: Petr Svetr#4490
+author: František Vůjtek
+email: frantisekvujtek@seznam.cz
+discord: fere_91948
 """
 
 import pprint
 import random
+import time
+
 
 
 line = "-" * 40
@@ -15,22 +17,23 @@ line = "-" * 40
 
 def duplicities(code):
     """
-    Converts 4 Digit input code to a string to compare the values
+    Converts 4 Digit input code to a string to and it compares the values
     """
     string = str(code)
 
     if len(string) != len(set(string)):
         value = False
-        print("Duplicities are not allowed, Enter unique numbers")
+        if code != random_number: # If random number generates number with duplicated numbers in code, skip it and print it only when user enters duplicated numbers.
+            print("Duplicities are not allowed, Enter unique numbers")
     else:
         value = True
 
-    # It compares if the lenght of the strings is the same or not
+    # It compares if the lenght of the strings is the same or not and checks for duplicities
     return value
 
 def control(code):
     """
-    Checks if entered input is 4 digits long and if it´s and integer
+    Checks if entered input is 4 digits long and if it´s an integer
     """
 
     if len(code) < 4 or len(code) > 4 and code.isdigit():
@@ -73,13 +76,13 @@ def cows(code1, code2):
     bulls = []
     cows = []
 
-    for i, x in enumerate(guess_code):
+    for i, x in enumerate(guess_code): # Indexing user attempts to compare values with Random generated code
         check_c1.append(x)
 
-    for i, y in enumerate(random_number):
+    for i, y in enumerate(random_number): # Indexing Random generated code to compare values with user´s attempts
         check_c2.append(y)
 
-    while True:
+    while True: # Indexing Lists of those 2 codes and compares if the positions and numbers are common or not and if yes, it adds it to the Lists
         for i, x in enumerate(check_c1):
             for ix, y in enumerate(check_c2):
                 if ix == i and x == y:
@@ -87,51 +90,61 @@ def cows(code1, code2):
                 elif x in check_c2 and not ix == i and x == y:
                     cows.append(y)
 
-        if len(bulls) == 1 and len(cows) == 1:
+        if len(bulls) == 1 and len(cows) == 1: # Bulls and Cows mechanisms which returns the output to the user if he guessed the positions or numbers.
             print(f"{len(bulls)} Bull, {len(cows)} Cow")
-        elif len(bulls) != 1 and len(cows) != 1:
+        elif len(bulls) != 1 and len(bulls) != 4 and len(cows) != 1: # If user guess 4 correct numbers and 4 correct positions, Don´t return any print about Bulls and Cows
             print(f"{len(bulls)} Bulls, {len(cows)} Cows")
         if len(bulls) == 1 and len(cows) != 1:
             print(f"{len(bulls)} Bull, {len(cows)} Cows")
-        elif len(bulls) != 1 and len(cows) == 1:
+        elif len(bulls) != 1  and len(cows) == 1:
             print(f"{len(bulls)} Bulls, {len(cows)} Cow")
             
+        if len(bulls) == 4:  # If the Value it´s False, End the Loop - False is when There are 4 Correct numbers and positions
+            value = False
+            print(" #### You won, Correct number is: ", random_number, " ####")
+        
+        else: # If its True, Loop it until False
+            value = True
+            break
         break
 
-    return
+    return value
 
 # Presentation and rules
 print("Hi There!")
 print(line)
 print("""
-        I Have generated a random 4 digit code for you.
-        Let´s play Bulls & Cows game.
+    I Have generated a random 4 digit code for you.
+    Let´s play Bulls & Cows game.
+    Basic Rules: 
+    - The Code can´t start with 0
+    - Numbers in the Code canno´t be twice
+    - The Code must be 4 Digits long
 """)
 print(line)
 
-    
-
-# generates random 4 Digits number for the game & It does not starts with 0
-while True:
+while True: # generates random 4 Digits number for the game & It does not starts with 0
     random_number = random.randrange(1000, 9999)
-    if duplicities(random_number) is False:
+    if duplicities(random_number) is True:
         break
     else:
         continue
 print(random_number)
 
-while True:
-    first_try = input("Enter a 4 Digit Number: ")
+while True: # Checks if user can guess the number on his first try, if not, continue in next loop
+    first_try = input("Enter a 4 Digit Number: ") # 
+    start_time = time.time()
     if control(first_try) == False:
         continue
     elif nozero(first_try) == False:
         continue
     elif duplicities(first_try) == False:
         continue
-    else:
-        cows(first_try, random_number)
+    elif cows(first_try, random_number) == False:
+            break
+        
 
-    while True:
+    while True: # Checks if user guess the number in the game
         code = input(">>>: ")
         if control(code) == False:
             continue
@@ -139,27 +152,20 @@ while True:
             continue
         elif duplicities(code) == False:
             continue
-        else:
-            cows(code, random_number)
+        elif cows(code, random_number) == False:
+                break
+    break
+
+for i in range(100000): # Measures the Elapsed time when users enters first attempt
+    result = i * i
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(line)
+print("You have guessed the code in: ", elapsed_time, "Seconds !")
 
 
 
-
-while True:
-    first_try = input("Enter a 4 Digits Number: ")
-    if control(first_try) == False:
-        continue
-    while True:
-        number = input("Guess: ")
-        if control(number) == False:
-            continue
-        if number or first_try == random_number: # Correct answer wins the game
-            print("Correct, You have guessed the right number!")
-            break
-        else:
-            number is not random_number
-            print("Nope, continue")
-            continue
 
 
 
